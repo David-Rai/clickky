441f12c091137a4f84d384b077ccce66e8c19c7e
david-rai <davidkhaling123@gmail.com>
2025-08-31 23:33:49 +0545

    PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+  <html lang="en">
        <Head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// next.config.js
+const withPWA = require("next-pwa")({
+  dest: "public", // folder where service worker will be generated
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
+
 export default nextConfig;
